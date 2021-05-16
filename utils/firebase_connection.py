a1c153dbdc095d23a6a20448c32b01fda7ef0d16
Niklas <niklas.kueppers@siemens.com>
2021-05-16 13:07:13 +0200

    fix: key path

--- utils/firebase_connection.py
+++ utils/firebase_connection.py
@@ -4,6 +4,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+abspath=os.path.abspath(__file__)
+
 def init():
   '''
   initializes database with credential found in folder key.
@@ -13,7 +15,8 @@ def init():
   returns:
     db: database object for further usage
   '''
-  cred = credentials.Certificate(os.path.join('utils', 'key', 'helium-data-firebase-adminsdk-7ehtd-b2a18d49c8.json'))
+  
+  cred = credentials.Certificate(os.path.join(abspath, '..', 'key', 'helium-data-firebase-adminsdk-7ehtd-b2a18d49c8.json'))
 
   firebase_admin.initialize_app(cred, {'projectId': 'helium-data'})
 
