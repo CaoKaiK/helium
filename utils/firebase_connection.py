30ccfe2eaa09a793ef102171fad123a7aef1eabe
Niklas <niklas.kueppers@siemens.com>
2022-03-20 14:34:21 +0100

    ext: comment

--- utils/firebase_connection.py
+++ utils/firebase_connection.py
@@ -4,23 +4,20 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+# define folder for firebase key
 abspath = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(abspath, 'key', 'helium-data-firebase-adminsdk-7ehtd-b2a18d49c8.json')
 
 def init():
   '''
-  initializes database with credential found in folder key.
-
-  projectId connects to helium-data
+  initializes database with credential found in folder 'key'.
 
   returns:
     db: database object for further usage
   '''
   
   cred = credentials.Certificate(file_path)
-
   firebase_admin.initialize_app(cred, {'projectId': 'helium-data'})
-
   db = firestore.client()
 
   return db
