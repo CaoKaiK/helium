3e66c3a5164745f3d133606ed45f9654f9a5f12c
Niklas <niklas.kueppers@siemens.com>
2022-03-20 14:47:15 +0100

    ext: comments

--- utils/balance_fifo.py
+++ utils/balance_fifo.py
@@ -1,7 +1,20 @@
-
-
 def run_balance(activities_ref, balance_height, last_balance):
-  # get activities later than latest balance run
+  '''
+  updates activities with a running balance. Starting from the last known balance.
+
+  args:
+    activities_ref: firebase reference to all activities
+    balance_height: int with height of last commited balance
+    last_balance: int representing the last commited balance
+
+  returns:
+    last_balance: int representing the balance in bones
+    last_activity: dict of last activity
+    last_fee: dict of last activity with a fee
+    
+  '''
+  
+  # get all activities from firebase that are later than latest balance run
   activities = activities_ref.where(u'height', u'>', balance_height).get()
 
   last_fee = {}
@@ -15,20 +28,22 @@ def run_balance(activities_ref, balance_height, last_balance):
       amount = activity.get('amount', 0)
       fee_hnt = activity.get('fee_hnt', 0)
 
+      # save activity if it has a positive fee
       if fee_hnt > 0:
         last_fee = activity
       
-
+      # calculate balance
       last_balance += amount - fee_hnt
 
-      # update activity
+      # update activity with current balance
       activity_ref = activities_ref.document(f'{height}_{hash_act}')
-
       activity_ref.update({u'current_balance': last_balance})
 
   else:
     activity = {}
+  
+  last_activity = activity
 
-  return last_balance, activity, last_fee
+  return last_balance, last_activity, last_fee
 
 
