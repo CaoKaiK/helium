13d12da7ec008b1585a82f39d5217335efde0b5e
Niklas Cao <niklas.cao@siemens-energy.com>
2022-08-09 18:53:51 +0200

    ext: juli

--- export_month.py
+++ export_month.py
@@ -14,7 +14,7 @@ account_list = [u'C&R']#, u'Temporary']
 
 # month
 eval_year = 2022  
-eval_month = 6
+eval_month = 7
 
 for account in account_list:
   # load config file and stream wallets
--- verify_export.py
+++ verify_export.py
@@ -58,6 +58,7 @@ export_list = [
   'datev_C&R_2022-4.xlsx',
   'datev_C&R_2022-5.xlsx',
   'datev_C&R_2022-6.xlsx',
+  'datev_C&R_2022-7.xlsx',
 ]
 
 export_df = pd.DataFrame()
